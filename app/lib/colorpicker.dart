9eb9430586572580b35cbbbd953d16ea1d58f5c3
barna_morvai <morvaibarna@gmail.com>
2021-04-21 16:36:52 +0200

    Update colorpicker.dart

--- app/lib/colorpicker.dart
+++ app/lib/colorpicker.dart
@@ -4,93 +4,106 @@ import 'main.dart';
 import 'package:firebase_database/firebase_database.dart';
 
 class CPHome extends StatelessWidget {
-  // CPHome({Key key, @required this.brightness,}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container(child: ColorPickerWidget());
+    return MaterialApp(home: ColorPickerWidget());
   }
 }
 
+class SizeConfig {
+  static MediaQueryData _mediaQueryData;
+  static double screenWidth;
+  static double screenHeight;
+  static double blockSizeHorizontal;
+  static double blockSizeVertical;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+    blockSizeHorizontal = screenWidth / 100;
+    blockSizeVertical = screenHeight / 100;
+  }
+}
+
+final databaseReference = FirebaseDatabase.instance.reference();
+
 class ColorPickerWidget extends StatefulWidget {
-  //const ColorPickerWidget({Key key}) : super(key: key);
+  const ColorPickerWidget({Key key}) : super(key: key);
   @override
   _ColorPickerState createState() => _ColorPickerState();
 }
 
 class _ColorPickerState extends State<ColorPickerWidget> {
-  Color pickerColor = Colors.red;
+  Color _pickerColor;
+  void changeColor(Color color) {
+    _pickerColor = color;
+    databaseReference.update({'Br': _pickerColor.alpha});
+    // databaseReference.child("Színek").update({'Red': _pickerColor.red});
+    // databaseReference.child("Színek").update({'Green': _pickerColor.green});
+    // databaseReference.child("Színek").update({'Blue': _pickerColor.blue});
+    // databaseReference.update({
+    //   'SetUp': 'szín' +
+    //       ' ' +
+    //       _pickerColor.red.toString() +
+    //       ' ' +
+    //       _pickerColor.green.toString() +
+    //       ' ' +
+    //       _pickerColor.blue.toString()
+    // });
+  }
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: Column(children: [
-          SizedBox(
-            height: 200,
-          ),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
           Container(
-            color: Colors.white,
-            child: ColorPicker(
-              pickerColor: pickerColor,
-              onColorChanged: changeColor,
-              showLabel: false,
-              pickerAreaHeightPercent: 0.8,
-              enableAlpha: false,
-              pickerAreaBorderRadius: BorderRadius.circular(33),
+            child: FutureBuilder(
+              future: databaseReference.child("Színek").once(),
+              builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
+                List lists = [];
+                Map<dynamic, dynamic> values = snapshot.data.value;
+                values.forEach((key, values) {
+                  lists.add(values);
+                });
+                _pickerColor = Color.fromRGBO(lists[0], lists[2], lists[1], 1);
+                return Container(
+                    child: ColorPicker(
+                  displayThumbColor: true,
+                  pickerColor: _pickerColor,
+                  onColorChanged: changeColor,
+                  showLabel: false,
+                  pickerAreaHeightPercent: 1,
+                  colorPickerWidth: SizeConfig.blockSizeHorizontal * 70,
+                  enableAlpha: true,
+                  portraitOnly: true,
+                  pickerAreaBorderRadius: BorderRadius.circular(50),
+                ));
+              },
             ),
           ),
-          OutlineButton(
-              child: Text('Set'),
-              onPressed: () {
-                databaseReference.update({'red': pickerColor.red});
-                databaseReference.update({'green': pickerColor.green});
-                databaseReference.update({'blue': pickerColor.blue});
-                databaseReference.update({
-                  'SetUp': 'szín' +
-                      ' ' +
-                      pickerColor.red.toString() +
-                      ' ' +
-                      pickerColor.green.toString() +
-                      ' ' +
-                      pickerColor.blue.toString()
-                });
-              }),
-          OutlineButton(
-              child: Text('Back'),
-              color: Colors.white,
-              textColor: Colors.black,
-              onPressed: () {
-                Navigator.push(context, FadeRoute(page: MyApp()));
-              })
-        ]));
+          OutlineButton(child: Text('Set'), onPressed: onPressed),
+        ],
+      ),
+    );
   }
 
-  void changeColor(Color color) {
-    setState(() => pickerColor = color);
+  void onPressed() {
+    databaseReference.child("Színek").update({'Red': _pickerColor.red});
+    databaseReference.child("Színek").update({'Green': _pickerColor.green});
+    databaseReference.child("Színek").update({'Blue': _pickerColor.blue});
+
+    databaseReference.update({
+      'SetUp': 'szín' +
+          ' ' +
+          _pickerColor.red.toString() +
+          ' ' +
+          _pickerColor.green.toString() +
+          ' ' +
+          _pickerColor.blue.toString()
+    });
   }
 }
-
-class FadeRoute extends PageRouteBuilder {
-  final Widget page;
-  FadeRoute({this.page})
-      : super(
-          pageBuilder: (
-            BuildContext context,
-            Animation<double> animation,
-            Animation<double> secondaryAnimation,
-          ) =>
-              page,
-          transitionsBuilder: (
-            BuildContext context,
-            Animation<double> animation,
-            Animation<double> secondaryAnimation,
-            Widget child,
-          ) =>
-              FadeTransition(
-            opacity: animation,
-            child: child,
-          ),
-        );
-}
-
-final databaseReference = FirebaseDatabase.instance.reference();
